0d5a40addd963f0ba8cbdc71bb114f58825bf40d
chris <chris.c.slaughter@gmail.com>
2018-10-05 13:14:00 -0700

    Added support for Litecoin nodes.

--- custody/admin.py
+++ custody/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Currency, Node
 
+class NodeInline(admin.TabularInline):
+  model = Node
+  fk_name = 'currency'
+
 class CurrencyAdmin(admin.ModelAdmin):
   model = Currency
   list_display = ('symbol', 'name')
+  inlines = (NodeInline,)
 
 admin.site.register(Currency, CurrencyAdmin)
