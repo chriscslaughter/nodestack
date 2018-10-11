87b9d87c21a498bb320947e9fdcf328386a6c58f
chris <chris.c.slaughter@gmail.com>
2018-10-11 12:16:35 -0700

    Added lib/ to the folder (can fix distribution later)

---
+++ lib/rpc.py
@@ -0,0 +1,29 @@
+class RPCException(Exception):
+    pass
+
+class RPC(object):
+    def __init__(self, address, username, password):
+        self.address = address
+        self.username = username
+        self.password = password
+    def make_call(self, method, params=[]):
+        import requests, json
+        url = "http://{username}:{password}@{address}/".format(
+            username=self.username, 
+            password=self.password, 
+            address=self.address
+        )
+        headers = {
+            'content-type': 'application/json'
+        }
+        data = {
+            "method": method,
+            "params": params,
+            "jsonrpc": "1.0"
+        }
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        response = response.json()
+        if response['error'] == None:
+            return response['result']
+        else:
+            raise RPCException(response['error'])
