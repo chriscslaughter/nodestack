600b55fef0b9c1f8481116101a404ad5cf389a55
tipu <danial@samsa.ai>
2018-10-12 15:32:11 -0700

    Adding settings example and switching from sqlite to psql.

--- nodestack/settings.py
+++ nodestack/settings.py
@@ -71,18 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'nodestack.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -120,3 +108,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from nodestack.settings_env import *
