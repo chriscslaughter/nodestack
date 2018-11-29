d85320c66b4ad6100be1f6b537b2a72e9ba50ede
tipu <danial@samsa.ai>
2018-11-28 16:22:26 -0800

    Tabular admin stuff working.

--- coldstoragetransfers/admin.py
+++ coldstoragetransfers/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from coldstoragetransfers.models import TransferRequest, TransferRequestSignature
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+class TransferRequestSignatureInline(admin.TabularInline):
+    model = TransferRequestSignature
+
+class TransferRequestAdmin(admin.ModelAdmin):
+    inlines = [
+        TransferRequestSignatureInline
+    ]
+    model = TransferRequest
+    # exclude = ('user_agent', 'ip_address', 'user')
+    readonly_fields = ('user_agent', 'ip_address')
+
+    def get_changeform_initial_data(self, request):
+        return {
+            'ip_address': get_client_ip(request),
+            # 'user_agent': request.META['HTTP_USER_AGENT']
+        }
+
+    def user_agent(self, instance):
+        return mark_safe("<span class='errors'>I can't determine this address.</span>")
+
+    user_agent.short_description = 'short desc'
+
+admin.site.register(TransferRequest, TransferRequestAdmin)
--- coldstoragetransfers/models.py
+++ coldstoragetransfers/models.py
@@ -10,19 +10,6 @@ class TransferRequest(models.Model):
     amount = models.DecimalField(max_digits=32, decimal_places=8)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        """
-        I can't move this into the `clean` method because on object creation,
-        I never have raw access to the instance due to how ModelForm works. 
-        As a result, `clean` always fails because `self.user` is never set in
-        the form.
-        """
-        if not self.user.is_superuser:
-            raise ValidationError({
-                'user': ValidationError(_('This user can\'t create transfer requests.'), code='insufficient_permissions'),
-            })
-        super().save(*args ,**kwargs)
-
 class TransferRequestSignature(models.Model):
     user = models.ForeignKey(User, related_name='transfer_request_signatures', on_delete=models.DO_NOTHING)
     transfer_request = models.ForeignKey(TransferRequest, related_name='signatures', on_delete=models.DO_NOTHING)
