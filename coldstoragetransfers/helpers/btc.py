fada97144ca393bfb41893bf7a182d08f921a0ee
tipu <danial@samsa.ai>
2018-12-19 17:55:26 -0800

    Shortening default fee.

--- coldstoragetransfers/helpers/btc.py
+++ coldstoragetransfers/helpers/btc.py
@@ -69,7 +69,7 @@ class BTCHelper:
 
     def generate_fee(self):
         # https://gist.github.com/dabura667/1bb77d63d38bfd99a0ce453db74e0115
-        return Decimal(0.001).quantize(DEFAULT_ZERO)
+        return Decimal(0.0001).quantize(DEFAULT_ZERO)
 
     def send_raw_transaction(self, transaction):
         txid = self.rpc.make_call('sendrawtransaction', [transaction])
